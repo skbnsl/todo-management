e83d65b8dfbb979dea5dd337f151f30500b91fc4
skbnsl <bansalsagar13@gmail.com>
2024-11-11 18:46:24 +0530

    some changes in addTodo

--- src/main/java/net/javaguides/todo/dto/TodoDto.java
+++ src/main/java/net/javaguides/todo/dto/TodoDto.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TodoDto {
 
-    private Long id;
     private String title;
     private String description;
     private boolean completed;
