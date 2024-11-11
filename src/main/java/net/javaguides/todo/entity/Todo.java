36f2f40775607b5e4dade80094873811577524c1
skbnsl <bansalsagar13@gmail.com>
2024-11-11 17:31:39 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/todo/entity/Todo.java
@@ -0,0 +1,27 @@
+package net.javaguides.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title",nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+    private boolean completed;
+
+
+
+}
