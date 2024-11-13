d0beadcc529ef610ad0b72ba7df538c31165a12e
skbnsl <bansalsagar13@gmail.com>
2024-11-13 19:10:20 +0530

    jwt authentication

--- src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
@@ -1,5 +1,8 @@
 package net.javaguides.todo.config;
 
+import net.javaguides.todo.security.JwtAuthenticationEntryPoint;
+import net.javaguides.todo.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,10 +18,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SpringSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -41,6 +52,12 @@ public class SpringSecurityConfig {
                     authorize.requestMatchers(HttpMethod.GET,"/api/auth/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
+
+        http.exceptionHandling(exception -> exception
+                .authenticationEntryPoint(authenticationEntryPoint));
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
